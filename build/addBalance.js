52dc49f0c9135f1c796d5e954d85161f23d996ad
caiobrian <caiogomeslog@gmail.com>
2019-12-06 16:10:13 -0300

    validação de criar conta feita.

--- build/addBalance.js
+++ build/addBalance.js
@@ -8,9 +8,19 @@ const fileName = 'users.json';
 const addBalance = (name, cpf, value) => {
     const usersDataJSON = fs.readFileSync(fileName).toString();
     const mutableUsersData = JSON.parse(usersDataJSON);
-    mutableUsersData.map((user) => user.name === name && user.cpf === cpf ? user.balance = user.balance + value : null);
+    const checkValidUser = mutableUsersData.filter((user) => {
+        return user.name === name && user.cpf === cpf;
+    }).length;
+    if (checkValidUser <= 0) {
+        mutableUsersData.map((user) => user.name === name && user.cpf === cpf
+            ? (user.balance = user.balance + value)
+            : null);
+    }
+    else {
+        console.log('Não foi possivel encontrar usuário');
+    }
     const userNewBalanceAsJSON = JSON.stringify(mutableUsersData);
     fs.writeFileSync(fileName, userNewBalanceAsJSON);
 };
-console.log(addBalance(nameUser, cpf, value));
+addBalance(nameUser, cpf, value);
 //# sourceMappingURL=addBalance.js.map
